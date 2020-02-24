49822160dfef289c896cc177ade7c390d5409903
Isuru Fernando <isuruf@gmail.com>
2020-02-24 05:10:19 +0000

    check again

--- check_fp.c
+++ check_fp.c
@@ -11,10 +11,12 @@ int main() {
   unsigned int orig_mxcsr = _mm_getcsr();
   printf("original value from _mm_getcsr %x\n", orig_mxcsr);
   printf("original value from fgetenv %lx\n", f.__mxcsr);
-  f.__mxcsr |= 0x8040;
-  //_mm_setcsr(orig_mxcsr | 0x8040);
+  //f.__mxcsr |= 0x8040;
+  _mm_setcsr(orig_mxcsr | 0x8040);
   //_controlfp(_DN_FLUSH, _MCW_DN);
-  int ret = fesetenv(&f);
+  //int ret = fesetenv(&f);
   unsigned int new_mxcsr = _mm_getcsr();
   printf("new value from _mm_getcsr %x\n", new_mxcsr);
+  fegetenv(&f);
+  printf("new value from fgetnev %x\n", f.__mxcsr);
 }
